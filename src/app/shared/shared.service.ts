3e21228f29fbec29aea5c12b6b100f63eba3158c
waqas <m.waqas@fam-llc.com>
2026-06-10 01:29:42 +0500

    remove secret

--- src/app/shared/shared.service.ts
+++ src/app/shared/shared.service.ts
@@ -6,8 +6,5 @@ import { Injectable } from '@angular/core';
 export class SharedService {
   API_KEY = "sk_live_123456789abcdef"
   googleApiKey = "AIzaSyBk_XKw9b45q5z1v2w3x4y5z6a7b8c9d0e"    
-
-
-  githubToken = "ghp_1234567890abcdefghijklmnopqrstuvwxyz";
   constructor() { }
 }
