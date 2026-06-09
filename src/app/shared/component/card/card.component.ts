bf1f1936a1b2381aed22c5df69b09b14fd41dbc6
waqas <m.waqas@fam-llc.com>
2026-06-10 02:10:48 +0500

    check again

--- src/app/shared/component/card/card.component.ts
+++ src/app/shared/component/card/card.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-cards',
+  selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
