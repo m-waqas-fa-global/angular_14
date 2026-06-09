e3a57693d366ce3e9a871918ce6fcffe4006ea24
waqas <m.waqas@fam-llc.com>
2026-06-10 00:31:06 +0500

    Commiting secret Keys.

--- src/app/shared/shared.service.ts
+++ src/app/shared/shared.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class SharedService {
-
+  API_KEY = "sk_live_123456789abcdef"
+  googleApiKey = "AIzaSyBk_XKw9b45q5z1v2w3x4y5z6a7b8c9d0e"
   constructor() { }
 }
