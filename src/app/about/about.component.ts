1a27174400f78b4e6a046c9356febdb8b8fe9528
waqas <m.waqas@fam-llc.com>
2026-06-10 02:28:01 +0500

    Added New Files in About.comp

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,7 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { ButtonModule } from 'primeng/button';
+import { API_KEY } from './data';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -15,6 +16,7 @@ export class AboutComponent implements OnInit {
   name = 'About Component';
 
   ngOnInit(): void {
+    console.log(API_KEY);
     this.broadChannel.onmessage = (message) => {
       console.warn("About Component received message: ", message);
     }
