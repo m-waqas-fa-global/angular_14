eae44bc0d355dbfea32be93d287786dc60ad1e7c
waqas <m.waqas@fam-llc.com>
2026-06-10 02:09:26 +0500

    made new changes

--- src/app/shared/shared.service.ts
+++ src/app/shared/shared.service.ts
@@ -3,8 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class SharedService {
-  API_KEY = "sk_live_123456789abcdef"
-  googleApiKey = "AIzaSyBk_XKw9b45q5z1v2w3x4y5z6a7b8c9d0e"    
+export class SharedService {  
   constructor() { }
 }
